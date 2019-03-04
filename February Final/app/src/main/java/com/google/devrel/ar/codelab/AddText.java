c1b3a050ece2c75d155e7060182f339494466429
rmoktader <rmoktader@cherrycreekschools.org>
2019-03-04 10:03:18 -0700

    cleaned up some code and button functionality

--- February Final/app/src/main/java/com/google/devrel/ar/codelab/AddText.java
+++ February Final/app/src/main/java/com/google/devrel/ar/codelab/AddText.java
@@ -2,11 +2,14 @@ package com.google.devrel.ar.codelab;
 
 import android.Manifest;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Address;
+import android.location.Criteria;
 import android.location.LocationProvider;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.provider.Settings.Secure;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -30,13 +33,22 @@ import android.location.Geocoder;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
+import android.support.v7.app.AlertDialog;
 import android.widget.Toast;
+
 
 import java.util.List;
 import java.util.Locale;
 import java.util.UUID;
 
 public class AddText extends AppCompatActivity {
+
+    LocationManager locationManager;
+    double longitudeBest, latitudeBest;
+    double longitudeGPS, latitudeGPS;
+    double longitudeNetwork, latitudeNetwork;
+
+    LocationListener locationListener;
 
     @Override
     public void onStart()
@@ -47,8 +59,7 @@ public class AddText extends AppCompatActivity {
         Log.i("GOTHEREGS", "started GS");
     }
 
-    LocationManager locationManager;
-    LocationListener locationListener;
+
 
 //    @Override
     public void onProviderDisabled(String provider) {
@@ -64,28 +75,6 @@ public class AddText extends AppCompatActivity {
                 Toast.LENGTH_SHORT).show();
     }
 
-    public Location checkPermissionLocation(){
-        boolean permission;
-        Location location;
-        if (ContextCompat.checkSelfPermission(getApplicationContext(),
-                android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-            permission = false;
-            location = null;
-            ActivityCompat.requestPermissions(
-                    this, new String[]{android.Manifest.permission.ACCESS_FINE_LOCATION,},
-                    101);
-
-
-        }
-        else{
-            permission = true;
-            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-            onProviderEnabled(LocationManager.NETWORK_PROVIDER);
-//            location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
-        }
-        return location;
-    }
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
@@ -93,7 +82,7 @@ public class AddText extends AppCompatActivity {
         setContentView(R.layout.activity_add_text);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        locationManager =  (LocationManager)getSystemService(LOCATION_SERVICE);
+//        locationManager =  (LocationManager)getSystemService(Context.LOCATION_SERVICE);
 
 
         GSAndroidPlatform.initialise(this, "u374201md1E4", "ktbBEnAi7UjgzEFdlY8s9E892AqZoVnR", "device", false, true);
@@ -135,13 +124,13 @@ public class AddText extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Log.i("GOTHERECLICK", "button");
-                Location location = checkPermissionLocation();
-//                boolean permission = checkPermissionLocation();
+                Location location = toggleBestUpdates();
+                Log.i("GOTHERECLICK", ""+location);
+
                 double lat;
                 double lon;
                 double bearing;
 
-//                location = locationManager.getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
                 if (location != null) {
                     lat = location.getLatitude();
 
@@ -159,7 +148,7 @@ public class AddText extends AppCompatActivity {
                         .setEventKey("SAVE_GEO_MESSAGE")
                         .setEventAttribute("LAT", ""+ lat )
                         .setEventAttribute("LON", "" + lon)
-                        .setEventAttribute("TEXT", "android connect")
+                        .setEventAttribute("TEXT", "gs connection")
                         .send(new GSEventConsumer<GSResponseBuilder.LogEventResponse>()
                         {
                             @Override
@@ -177,7 +166,6 @@ public class AddText extends AppCompatActivity {
 
                         });
 
-
                 Log.i("GOTHERE", "about to start");
                 startActivity(new Intent(AddText.this, MainActivity.class));
                 Log.i("GOTHERE", "started activity");
@@ -185,5 +173,190 @@ public class AddText extends AppCompatActivity {
             }
         });
     }
+
+    private boolean checkLocation(){
+        if(!isLocationEnabled()){
+            showAlert();
+        }
+        return isLocationEnabled();
+    }
+
+    private void showAlert() {
+        final AlertDialog.Builder dialog = new AlertDialog.Builder(this);
+        dialog.setTitle("Enable Location")
+                .setMessage("Your Locations Settings is set to 'Off'.\nPlease Enable Location to " +
+                        "use this app")
+                .setPositiveButton("Location Settings", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                        Intent myIntent = new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        startActivity(myIntent);
+                    }
+                })
+                .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface paramDialogInterface, int paramInt) {
+                    }
+                });
+        dialog.show();
+    }
+
+    private boolean isLocationEnabled() {
+        if (LocationManager.GPS_PROVIDER!=null && LocationManager.NETWORK_PROVIDER!=null  && locationManager!=null) {
+            return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                    locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER);
+        }
+        return false;
+    }
+
+    public void toggleGPSUpdates() {
+        if(!checkLocation())
+            return;
+
+        else {
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // Check Permissions Now
+                int REQUEST_LOCATION = 2;
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_LOCATION);
+            } else {
+                // permission has been granted, continue as usual
+                locationManager.requestLocationUpdates(
+                        LocationManager.GPS_PROVIDER, 2 * 60 * 1000, 10, locationListenerGPS);
+
+            }
+        }
+    }
+
+    public Location toggleBestUpdates() {
+        if(!checkLocation()){
+            return null;
+        }
+        else {
+            Criteria criteria = new Criteria();
+            criteria.setAccuracy(Criteria.ACCURACY_FINE);
+            criteria.setAltitudeRequired(false);
+            criteria.setBearingRequired(false);
+            criteria.setCostAllowed(true);
+            criteria.setPowerRequirement(Criteria.POWER_LOW);
+            String provider = locationManager.getBestProvider(criteria, true);
+            Log.i("GOTHERELOC", "provider:"+ provider);
+            if(provider != null) {
+                if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                        != PackageManager.PERMISSION_GRANTED) {
+                    // Check Permissions Now
+                    Log.i("GOTHERELOC", "no permission");
+                    int REQUEST_LOCATION = 2;
+                    ActivityCompat.requestPermissions(this,
+                            new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                            REQUEST_LOCATION);
+
+                } else {
+                    Log.i("GOTHERELOC", "permission granted");
+                    // permission has been granted, continue as usual
+                    locationManager.requestLocationUpdates(provider, 2 * 60 * 1000, 10,
+                            locationListenerBest);
+//                    Toast.makeText(this, "Best Provider is " + provider, Toast.LENGTH_LONG).show();
+                    return locationManager.getLastKnownLocation(provider);
+                }
+            }
+        }
+        return null;
+    }
+
+    public void toggleNetworkUpdates(View view) {
+        if(!checkLocation())
+            return;
+
+        else {
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                    != PackageManager.PERMISSION_GRANTED) {
+                // Check Permissions Now
+                int REQUEST_LOCATION = 2;
+                ActivityCompat.requestPermissions(this,
+                        new String[]{Manifest.permission.ACCESS_FINE_LOCATION},
+                        REQUEST_LOCATION);
+            } else {
+                // permission has been granted, continue as usual
+                locationManager.requestLocationUpdates(
+                        LocationManager.NETWORK_PROVIDER, 60 * 1000, 10, locationListenerNetwork);
+                Toast.makeText(this, "Network provider started running", Toast.LENGTH_LONG).show();
+            }
+
+        }
+    }
+
+    private final LocationListener locationListenerBest = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            longitudeBest = location.getLongitude();
+            latitudeBest = location.getLatitude();
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+    private final LocationListener locationListenerNetwork = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            longitudeNetwork = location.getLongitude();
+            latitudeNetwork = location.getLatitude();
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+    private final LocationListener locationListenerGPS = new LocationListener() {
+        public void onLocationChanged(Location location) {
+            longitudeGPS = location.getLongitude();
+            latitudeGPS = location.getLatitude();
+
+        }
+
+        @Override
+        public void onStatusChanged(String s, int i, Bundle bundle) {
+
+        }
+
+        @Override
+        public void onProviderEnabled(String s) {
+
+        }
+
+        @Override
+        public void onProviderDisabled(String s) {
+
+        }
+    };
+
+
 
 }
