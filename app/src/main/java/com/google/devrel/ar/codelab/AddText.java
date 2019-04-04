d86c461f1576cac853be0b2d937cd472ee7fb971
rmoktader <rmoktader@cherrycreekschools.org>
2019-04-04 10:28:35 -0600

    Renamed MainActivity and related .xml files

--- app/src/main/java/com/google/devrel/ar/codelab/AddText.java
+++ app/src/main/java/com/google/devrel/ar/codelab/AddText.java
@@ -5,45 +5,27 @@ import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.location.Address;
 import android.location.Criteria;
-import android.location.LocationProvider;
 import android.os.Bundle;
 import android.provider.Settings;
-import android.provider.Settings.Secure;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.telephony.TelephonyManager;
 import android.util.Log;
 import android.view.Gravity;
 import android.view.View;
-import android.view.inputmethod.InputMethodManager;
 import android.widget.Button;
 
 import com.gamesparks.sdk.GSEventConsumer;
 import com.gamesparks.sdk.android.GSAndroidPlatform;
-import com.gamesparks.sdk.api.GSData;
 import com.gamesparks.sdk.api.autogen.GSResponseBuilder;
-import com.gamesparks.sdk.api.autogen.GSRequestBuilder.LogEventRequest;
-import com.gamesparks.sdk.api.autogen.GSResponseBuilder.LogEventResponse;
-import com.gamesparks.sdk.api.autogen.GSTypes.*;
-import android.location.Geocoder;
+
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.support.v7.app.AlertDialog;
 import android.widget.MultiAutoCompleteTextView;
-import android.widget.TextView;
 import android.widget.Toast;
-
-
-import java.util.List;
-import java.util.Locale;
-import java.util.UUID;
 
 public class AddText extends AppCompatActivity {
 
@@ -123,7 +105,7 @@ public class AddText extends AppCompatActivity {
                         });
 
                 Log.i("GOTHERE", "about to start");
-                startActivity(new Intent(AddText.this, MainActivity.class));
+                startActivity(new Intent(AddText.this, ArMainActivity.class));
                 Log.i("GOTHERE", "started activity");
 
             }
